968ad3d78a7a4ce07bdb95099c742a17eb7233a4
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2023-10-12 08:03:31 +0100

    cors

--- main/main.go
+++ main/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/HMUniversity/System/controllers"
 	"github.com/HMUniversity/System/modules/config"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
@@ -13,10 +14,14 @@ func main() {
 	app := fiber.New(fiber.Config{
 		CaseSensitive: true,
 		StrictRouting: false,
+		Prefork:       true,
 		ServerHeader:  "HMU Web Service",
 		AppName:       "HMU Core System",
 	})
 	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+	}))
 	controllers.RegisterControllers(app)
 	err := app.Listen(config.Get().ListenAddr)
 	if err != nil {
